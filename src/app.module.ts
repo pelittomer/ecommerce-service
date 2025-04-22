8a4944b3cb82f83bf0be9b38dca0cd156890ff86
pelittomer <pelittomer99@gmail.com>
2025-04-22 09:31:32 +0300

    chore: Configure initial environment and settings.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfig from './config/app';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppConfig } from './config/type';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig]
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService<AppConfig>) => ({
+        uri: configService.get('db.database_url', { infer: true })
+      }),
+      inject: [ConfigService]
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService<AppConfig>) => ({
+        secret: configService.get('auth.secret_key', { infer: true })
+      })
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,53 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import { AppConfig } from './config/type';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ConfigService } from '@nestjs/config';
+
+
+function setupSwagger(app) {
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce API')
+    .setDescription('API documentation for managing products, orders, and users in an e-commerce platform.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('App')
+  // Sets the global prefix for all API routes to '/api'
+  app.setGlobalPrefix('api')
+  //middleware
+  app.use(cookieParser())
+  app.use(helmet())
+  app.enableCors()
+  //swagger configuration
+  setupSwagger(app)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      validationError: {
+        target: true,
+        value: true,
+      },
+    })
+  )
+
+  app.useGlobalFilters(new HttpExceptionFilter())
+
+  const port = app.get(ConfigService<AppConfig, true>).get('api.port', { infer: true })
+  await app.listen(port)
+
+  logger.log(`Application started on port:${port}`)
 }
 bootstrap();
